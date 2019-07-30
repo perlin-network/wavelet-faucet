9e619c6bfc249d05e8e5491c0f37816223e760c3
Ryan <ryanswrt@gmail.com>
2019-07-30 20:19:51 +0800

    fix: don't ignore index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Faucet from './Faucet.jsx';
+import FaucetButton from './FaucetButton.jsx';
+import Modal from './Modal.jsx';
+import { mono } from './theme';
+
+export default { Faucet, Modal, FaucetButton };
